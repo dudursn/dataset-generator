c539e4271d206a7f4cefd189199343e560cdb5b6
Eduardo Nascimento <rogerrsn@tecgraf.puc-rio.br>
2022-11-26 23:55:30 -0300

    [Mod] Fix template

--- frontend/src/app/layouts/admin-layout/admin-layout.routing.ts
+++ frontend/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -1,21 +1,9 @@
 import { Routes } from '@angular/router';
 
 import { DashboardComponent } from '../../dashboard/dashboard.component';
-import { UserProfileComponent } from '../../user-profile/user-profile.component';
-import { TableListComponent } from '../../table-list/table-list.component';
-import { TypographyComponent } from '../../typography/typography.component';
-import { IconsComponent } from '../../icons/icons.component';
-import { MapsComponent } from '../../maps/maps.component';
-import { NotificationsComponent } from '../../notifications/notifications.component';
 import { HomeComponent } from 'app/home/home.component';
 
 export const AdminLayoutRoutes: Routes = [
     { path: 'home',           component: HomeComponent },
     { path: 'dashboard',      component: DashboardComponent },
-    { path: 'user-profile',   component: UserProfileComponent },
-    { path: 'table-list',     component: TableListComponent },
-    { path: 'typography',     component: TypographyComponent },
-    { path: 'icons',          component: IconsComponent },
-    { path: 'maps',           component: MapsComponent },
-    { path: 'notifications',  component: NotificationsComponent },
 ];
